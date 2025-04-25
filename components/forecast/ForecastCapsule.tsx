0d7bf1ca791ba9f30147e2f44f55b492f2116cf0
dorinhavardi <dorin.havardi@gmail.com>
2025-04-25 09:41:03 +0300

    capsule

---
+++ components/forecast/ForecastCapsule.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Forecast } from '../../models/Weather';
+import { Canvas, RoundedRect, Shadow } from '@shopify/react-native-skia';
+
+interface ForecastCapsuleProps {
+  forecast: Forecast;
+  width: number;
+  height: number;
+  radius: number;
+}
+
+const ForecastCapsule = ({
+  forecast,
+  width,
+  height,
+  radius,
+}: ForecastCapsuleProps) => {
+  return (
+    <View style={{ width, height }}>
+      <Canvas style={{ flex: 1 }}>
+        <RoundedRect
+          x={0}
+          y={0}
+          width={width}
+          height={height}
+          r={radius}
+          color={'rgba(72,49,157,1)'}
+        />
+        <Shadow dx={1} dy={1} blur={0} color={'rgba(255,255,255,0.25)'} inner />
+      </Canvas>
+    </View>
+  );
+};
+
+export default ForecastCapsule;
+
+const styles = StyleSheet.create({});
--- components/sheet/ForecastSheet.tsx
+++ components/sheet/ForecastSheet.tsx
@@ -3,6 +3,10 @@ import React, { useRef } from 'react';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import ForecastSheetBackground from './ForecastSheetBackground';
 import useApplicationDimensions from '../../hooks/useApplicationDimensions';
+import ForecastControl from './elements/ForecastControl';
+import Separator from './elements/Separator';
+import ForecastCapsule from '../forecast/ForecastCapsule';
+import { hourly } from '../../data/ForecastData';
 
 const ForecastSheet = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -10,6 +14,9 @@ const ForecastSheet = () => {
   const snapPoints = ['38.5%', '83%'];
   const firstSnapPoint = (height * parseFloat(snapPoints[0])) / 100;
   const cornerRadius = 44;
+  const capsuleRadius = 30;
+  const capsuleHeight = height * 0.17;
+  const capsuleWidth = width * 0.15;
 
   return (
     <BottomSheet
@@ -28,9 +35,16 @@ const ForecastSheet = () => {
         />
       )}
     >
-      <BottomSheetView style={{ flex: 1 }}>
-        <Text>Bottom Sheet Content</Text>
-      </BottomSheetView>
+      <>
+        <ForecastControl />
+        <Separator width={width} height={3} />
+        <ForecastCapsule
+          forecast={hourly[0]}
+          radius={capsuleRadius}
+          width={capsuleWidth}
+          height={capsuleHeight}
+        />
+      </>
     </BottomSheet>
   );
 };
